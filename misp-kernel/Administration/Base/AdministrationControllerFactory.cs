7503563f1aef891ed26394d40f268533f1fcfe75
DSimonPat <dsimonpat@kps-it.com>
2016-06-21 17:59:09 +0100

    ADD

---
+++ misp-kernel/Administration/Base/AdministrationControllerFactory.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Misp.Kernel.Application;
+using Misp.Kernel.Controller;
+using Misp.Kernel.Service;
+using Misp.Kernel.Domain;
+using Misp.Kernel.Administration.Base;
+using Misp.Kernel.Administration.Role;
+
+namespace Misp.Reconciliation.Base
+{
+    public class AdministrationControllerFactory : ControllerFactory
+    {
+
+        /// <summary>
+        /// Build a new instance of ReconciliationControllerFactory.
+        /// </summary>
+        /// <param name="applicationManager">Application manager</param>
+        public AdministrationControllerFactory(ApplicationManager applicationManager)
+            : base(applicationManager)
+        {
+            this.ServiceFactory = new AdministrationServiceFactory(applicationManager);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fonctionality"></param>
+        /// <returns></returns>
+        public override Controllable GetController(string fonctionality)
+        {
+            if (fonctionality == AdministrationFunctionalitiesCode.ADMINISTRATION_ROLE)
+            {
+                RoleBrowserController roleEditorController = new RoleBrowserController();
+                roleEditorController.ModuleName = Misp.Kernel.Administration.PlugIn.MODULE_NAME;
+                roleEditorController.Functionality = fonctionality;
+                roleEditorController.ApplicationManager = this.ApplicationManager;
+                roleEditorController.Service = ((AdministrationServiceFactory)ServiceFactory).GetRoleService();
+                return roleEditorController;
+            }
+
+            if (fonctionality == AdministrationFunctionalitiesCode.ADMINISTRATION_NEW_USER)
+            {
+                
+            }
+
+            if (fonctionality == AdministrationFunctionalitiesCode.ADMINISTRATION_NEW_USER)
+            {
+                
+            }
+            return null;
+        }
+
+    }
+}
---
+++ misp-kernel/Administration/Base/AdministrationServiceFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Misp.Kernel.Application;
+using Misp.Kernel.Service;
+using Misp.Kernel.Administration.Base;
+
+namespace Misp.Reconciliation.Base
+{
+    public class AdministrationServiceFactory : ServiceFactory
+    {
+
+        private RoleService roleService;
+
+        /// <summary>
+        /// Build a new instance of AdministrationServiceFactory.
+        /// </summary>
+        /// <param name="applicationManager">Application manager</param>
+        public AdministrationServiceFactory(ApplicationManager applicationManager)
+            : base(applicationManager) { }
+
+
+        /// <summary>
+        /// Gets RoleService
+        /// </summary>
+        public RoleService GetRoleService()
+        {
+            if (roleService == null)
+            {
+                roleService = new RoleService();
+                roleService.ResourcePath = AdministrationResourcePath.SECURITY_ROLE_RESOURCE_PATH;
+                roleService.GroupService = GetGroupService();
+                roleService.FileService = GetFileService();
+                roleService.ModelService = GetModelService();
+                configureService(roleService);
+            }
+            return roleService;
+        }
+        
+    }
+}
