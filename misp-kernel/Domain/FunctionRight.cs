f12a8db2049ef90a71e32eae23d617d9a797da1b
DSimonPat <dsimonpat@kps-it.com>
2016-06-22 17:07:30 +0100

    Add

---
+++ misp-kernel/Domain/FunctionRight.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misp.Kernel.Domain
+{
+  public class FunctionRight
+  {
+
+      public String function;
+
+      public Boolean view;
+
+      public Boolean edit;
+
+      public FunctionRight(String function, Boolean view, Boolean edit)
+      {
+          this.function = function;
+          this.view = view;
+          this.edit = edit;
+      }
+      
+      public static List<FunctionRight> generateDefaultFunction()
+      {
+          List<FunctionRight> functionRights = new List<FunctionRight>(0);
+          functionRights.Add(new FunctionRight("Initialisation", true, true));
+          functionRights.Add(new FunctionRight("InputTable", true, true));
+          functionRights.Add(new FunctionRight("Report", true, true));
+          functionRights.Add(new FunctionRight("TransformationTree", true, true));
+          functionRights.Add(new FunctionRight("Target", true, true));
+          functionRights.Add(new FunctionRight("StructuredReport", true, true));
+          functionRights.Add(new FunctionRight("AutomaticSourcing", true, true));
+          functionRights.Add(new FunctionRight("MultipleUplaod", true, true));
+          functionRights.Add(new FunctionRight("Design", true, true));
+          functionRights.Add(new FunctionRight("AutomaticTarget", true, true));
+          functionRights.Add(new FunctionRight("CombinedTranformationTree", true, true));
+          functionRights.Add(new FunctionRight("Load", true, true));
+          functionRights.Add(new FunctionRight("Clear", true, true));
+          functionRights.Add(new FunctionRight("Posting", true, true));
+          functionRights.Add(new FunctionRight("ReconciliationFilter", true, true));
+          functionRights.Add(new FunctionRight("Clear Launch", true, true));
+          functionRights.Add(new FunctionRight("Laod Launch", true, true));
+
+          return functionRights;
+        }
+    }
+}
