2053e162cf2bcb67526a686e95e5264d696e69c7
DSimonPat <dsimonpat@kps-it.com>
2016-06-22 11:23:12 +0100

    ADD

---
+++ misp-kernel/Service/UserService.cs
@@ -0,0 +1,78 @@
+﻿using Misp.Kernel.Domain;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misp.Kernel.Service
+{
+    public class UserService : Service<Misp.Kernel.Domain.User, Misp.Kernel.Domain.Browser.BrowserData>
+    {
+        #region Properties
+        /// <summary>
+        /// Le ModelService.
+        /// </summary>
+        public ModelService ModelService { get; set; }
+
+
+        #endregion
+
+        #region USER
+
+        /// <summary>
+        /// save role
+        /// </summary>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        public User saveUser(User user)
+        {
+            try
+            {
+                var request = new RestRequest(ResourcePath + "/save", Method.POST);
+                string json = Serialize(user);
+                request.RequestFormat = DataFormat.Json;
+                request.AddParameter("application/json", json, ParameterType.RequestBody);
+                RestResponse queryResult = (RestResponse)RestClient.Execute(request);
+                User usr = RestSharp.SimpleJson.DeserializeObject<User>(queryResult.Content);
+                return usr;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        
+
+        /// <summary>
+        /// get Role by oid
+        /// </summary>
+        /// <param name="oid"></param>
+        /// <returns></returns>
+        public User getUser(int oid)
+        {
+            try
+            {
+                var request = new RestRequest(ResourcePath + "/user/" + oid, Method.GET);
+                request.RequestFormat = DataFormat.Json;
+                RestResponse queryResult = (RestResponse)RestClient.Execute(request);
+                User u = RestSharp.SimpleJson.DeserializeObject<User>(queryResult.Content);
+                return u;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+        #endregion
+        
+        #region User
+        #endregion
+
+        #region Rights
+        #endregion
+
+    }
+}
