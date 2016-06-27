66e7a6378da9342428ba302c64498739078addde
DSimonPat <dsimonpat@kps-it.com>
2016-06-27 17:50:42 +0100

    ADD

---
+++ misp-kernel/Domain/Browser/ProfilBrowserData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misp.Kernel.Domain.Browser
+{
+    public class ProfilBrowserData : BrowserData
+    {
+
+        public bool active { get; set; }
+
+        public ProfilBrowserData() : base() { }
+
+        public ProfilBrowserData(BrowserData data) : base(data) { }
+
+    }
+}
