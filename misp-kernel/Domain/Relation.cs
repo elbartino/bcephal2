603075cb1ef341b01e042bf46618c549020b3ba1
tnjipmo <tnjipmo@kps-it.com>
2016-06-30 17:35:49 +0200

    Updates.

--- misp-kernel/Domain/Relation.cs
+++ misp-kernel/Domain/Relation.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 
 namespace Misp.Kernel.Domain
 {
     public class Relation : Persistent
     {
         public User owner { get; set; }
+
+        [ScriptIgnore]
         public User user { get; set; }
+
         public Role role { get; set; }
 
         public override int CompareTo(object obj)
