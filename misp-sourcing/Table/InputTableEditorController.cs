d282e95edf8c3dc565fe27374e54c25d892f20cb
DSimonPat <dsimonpat@kps-it.com>
2016-07-28 12:43:20 +0100

    Update

--- misp-sourcing/Table/InputTableEditorController.cs
+++ misp-sourcing/Table/InputTableEditorController.cs
@@ -2919,8 +2919,8 @@ namespace Misp.Sourcing.Table
         /// <returns></returns>
         protected override InputTable GetObjectByName(string name)
         {
-            //return ((InputTableSideBar)SideBar).InputTableGroup.InputTableTreeview.getInputTableByName(name);
-            return GetInputTableService().getByName(name);
+            return ((InputTableSideBar)SideBar).InputTableGroup.InputTableTreeview.getInputTableByName(name);
+            //return GetInputTableService().getByName(name);
         }
 
         protected override string getNewPageName(string prefix)
