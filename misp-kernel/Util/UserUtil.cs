d757b21c30e6dcaf2eb9d06879bf486514767070
thierry.njipmo@kps-it.com <thierry.njipmo@kps-it.com>
2016-08-05 07:45:51 +0200

    Updates.

---
+++ misp-kernel/Util/UserUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Misp.Kernel.Util
+{
+    public class UserUtil
+    {
+        public static bool validatePassword(String password1, String passwordConfirm)
+        {
+            return String.Compare(password1, passwordConfirm, false) == 0;
+        }
+
+        public static bool validateEmail(String email)
+        {
+            string strRegex = "^([a-zA-Z0-9_\\-\\.]+)@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.)|(([a-zA-Z0-9\\-]+\\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\\]?)$";
+            Regex re = new Regex(strRegex);
+            if (re.IsMatch(email))
+                return (true);
+            else
+                return (false);
+        }
+    }
+}
