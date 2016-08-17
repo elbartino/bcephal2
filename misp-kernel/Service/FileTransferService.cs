f9d7a9de492845fba25ff9cbe3d1ef8fef55f26c
Joseph <josephwambo@kps-it.com>
2016-08-17 10:22:54 +0100

    file transfer

--- misp-kernel/Service/FileTransferService.cs
+++ misp-kernel/Service/FileTransferService.cs
@@ -1,97 +1,68 @@
-﻿using Misp.Kernel.Domain;
-using Misp.Kernel.Domain.Browser;
+﻿using Misp.Kernel.Domain.Browser;
 using RestSharp;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 
 namespace Misp.Kernel.Service
 {
-    public class FileTransferService : Service<Persistent, BrowserData>
+    public class FileTransferService : Service<Domain.Persistent, BrowserData>
     {
 
-
-        /// <summary>
-        /// get Role by oid
-        /// </summary>
-        /// <param name="oid"></param>
-        /// <returns></returns>
-        public void download()
+        public bool uploadFile()
         {
             try
             {
-                var request = new RestRequest(ResourcePath + "/file", Method.GET);
-                byte[] bytes = RestClient.DownloadData(request);
-                string filePath = "D:\\cards-1.xlsx";
-                ByteArrayToFile(filePath, bytes);
+                string name = "cards";
+                string ext = ".xlsx";
+                string filePath = "D:\\" + name + ext;
+                var request = new RestRequest(ResourcePath + "/upload/" + name + "/" + ext, Method.POST);
+                request.AddHeader("Content-Type", "application/octet-stream");
+                request.RequestFormat = RestSharp.DataFormat.Json;
+                byte[] dataToSend = File.ReadAllBytes(filePath);
+                request.AddParameter("application/octet-stream", dataToSend, ParameterType.RequestBody);
+                IRestResponse response = RestClient.Execute(request);
+                JavaScriptSerializer Serializer = new JavaScriptSerializer();
+                Serializer.MaxJsonLength = int.MaxValue;
+                bool ok = Serializer.Deserialize<bool>(response.Content);
+                return ok;
             }
             catch (Exception e)
             {
-                e.ToString();
+                logger.Error("Unable to save Item.", e);
             }
-        }
-
-
-        public bool ByteArrayToFile(string _FileName, byte[] _ByteArray)
-        {
-            try
-            {
-                // Open file for reading
-                System.IO.FileStream _FileStream =
-                   new System.IO.FileStream(_FileName, System.IO.FileMode.Create,
-                                            System.IO.FileAccess.Write);
-                // Writes a block of bytes to this stream using data from
-                // a byte array.
-                _FileStream.Write(_ByteArray, 0, _ByteArray.Length);
-
-                // close file stream
-                _FileStream.Close();
-
-                return true;
-            }
-            catch (Exception _Exception)
-            {
-                // Error
-                Console.WriteLine("Exception caught in process: {0}",
-                                  _Exception.ToString());
-            }
-
-            // error occured, return false
             return false;
         }
 
-        //test envoi de fichier. code a supprimer !!!
-        public void sendExcelFile()
+
+
+        public bool downloadFile()
         {
-            //Kernel.Service.FileDirs fileDirs = this.FileService.GetFileDirs();
-            //string filePath = fileDirs.InputTableDir + "testTransfert.xlsx";
-            //try
-            //{
-            //    JavaScriptSerializer serializer = new JavaScriptSerializer();
-            //    var request = new RestRequest(ResourcePath + "/sends", Method.POST);
-            //    byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
-
-            //    /*Re-Ecriture du fichier pour se rassurer que le byte est correct*/
-            //    string filePath2 = fileDirs.InputTableDir + "Copy_testTransfert.xlsx";
-            //    System.IO.File.WriteAllBytes(filePath2, fileBytes);
-
-            //    /*Envoie du fichier au serveur*/
-            //    request.RequestFormat = DataFormat.Json;
-            //    serializer.MaxJsonLength = int.MaxValue;
-            //    string json = serializer.Serialize(fileBytes);
-            //    request.AddParameter("application/json", json, ParameterType.RequestBody);
-            //    var response = RestClient.ExecuteTaskAsync(request);
-            //    RestResponse queryResult = (RestResponse)response.Result;
-            //    bool resp = serializer.Deserialize<bool>(queryResult.Content);
-            //    return resp;
-            //}
-            //catch (Exception ex)
-            //{
-            //    return false;
-            //}
+            try
+            {
+                string name = "cards";
+                string ext = ".xlsx";
+                var request = new RestRequest(ResourcePath + "/download/" + name + "/" + ext, Method.GET);
+                request.AddHeader("Content-Type", "application/octet-stream");
+                request.RequestFormat = RestSharp.DataFormat.Json;                
+                byte[] data = RestClient.DownloadData(request);
+                string filePath = "D:\\Wtest2\\" + name + ext;
+                File.WriteAllBytes(filePath, data);
+                return true;
+            }
+            catch (Exception e)
+            {
+                logger.Error("Unable to save Item.", e);
+            }
+            return false;
         }
+
+
+        
 
     }
 }
