156d00071eab0b7a7f215b6c18bc63da53bcfee6
Joseph <josephwambo@kps-it.com>
2016-09-14 06:50:30 +0100

    update

---
+++ misp-kernel/Domain/Browser/BrowserDataPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misp.Kernel.Domain.Browser
+{
+    public class BrowserDataPage<B>
+    {
+        
+        public int pageSize { get; set; }
+
+        public int pageFirstItem { get; set; }
+
+        public int pageLastItem { get; set; }
+
+        public int totalItemCount { get; set; }
+
+        public int pageCount { get; set; }
+
+        public int currentPage { get; set; }
+
+        public List<B> rows { get; set; }
+
+        public BrowserDataPage()
+        {
+            rows = new List<B>(0);
+        }
+
+    }
+}
