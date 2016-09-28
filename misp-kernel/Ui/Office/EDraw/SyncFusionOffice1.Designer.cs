86f1022bfaae9d97d1ec0b6f9cd867afff2fbca3
DSimonPat <dsimonpat@kps-it.com>
2016-09-28 15:01:31 +0100

    create syncFusion files

---
+++ misp-kernel/Ui/Office/EDraw/SyncFusionOffice1.Designer.cs
@@ -0,0 +1,47 @@
+﻿using Syncfusion.XlsIO;
+namespace Misp.Kernel.Ui.Office.EDraw
+{
+    partial class SyncFusionOffice1
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ExcelEngine));
+            this.excelEngine1 = new ExcelEngine();
+            this.SuspendLayout();
+            this.Name = "SyncFusionExcel";
+            //IApplication   newExcelApp = new this.excelEngine1.Excel.Application();
+            //this.cont
+            ////this.Controls.Add(this.excelEngine1);
+            //components.Add(this.excelEngine1.Excel.Workbooks[0]);
+        }
+
+        private ExcelEngine excelEngine1;
+        #endregion
+    }
+}
