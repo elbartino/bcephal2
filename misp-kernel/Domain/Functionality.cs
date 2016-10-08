ab4dbceac4b548276cc87b1bf7999689f3ef19de
DSimonPat <dsimonpat@kps-it.com>
2016-10-08 13:04:45 +0100

    Add

--- misp-kernel/Domain/Functionality.cs
+++ misp-kernel/Domain/Functionality.cs
@@ -1,17 +1,13 @@
-﻿using Misp.Kernel.Application;
-using Misp.Kernel.Ui.Base.Menu;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
 
 namespace Misp.Kernel.Domain
 {
-    public class Functionality
+    class Functionality
     {
-
         public string code { get; set; }
         public string name { get; set; }
         List<Functionality> children { get; set; }
