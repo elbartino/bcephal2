7054ddcb99d9944b4d1277c1fe8dca9ce960f37c
thierry.njipmo@kps-it.com <thierry.njipmo@kps-it.com>
2016-10-10 14:51:38 +0200

    Updates.

--- misp-kernel/Domain/Functionality.cs
+++ misp-kernel/Domain/Functionality.cs
@@ -6,16 +6,17 @@ using System.Threading.Tasks;
 
 namespace Misp.Kernel.Domain
 {
-    class Functionality
+   public class Functionality
     {
         public string code { get; set; }
         public string name { get; set; }
-        List<Functionality> children { get; set; }
+        public List<Functionality> children { get; set; }
 
         public Functionality(string code, string name)
         {
             this.code = code;
             this.name = name;
+            children = new List<Functionality>(0);
         }
     }
 }
