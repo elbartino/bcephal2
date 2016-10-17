7ec81afb0409a1f7094e6945c96630381bf0c8d5
Joseph <josephwambo@kps-it.com>
2016-10-17 07:51:00 +0100

    Add LoopUserDialogTemplateData

---
+++ misp-kernel/Domain/LoopUserDialogTemplateData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misp.Kernel.Domain
+{
+    public class LoopUserDialogTemplateData
+    {
+        public String message { get; set; }
+        public String help { get; set; }
+        public List<Value> values { get; set; }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public LoopUserDialogTemplateData() {
+		    values = new List<Value>(0);
+	    }
+
+    }
+}
---
+++ misp-kernel/Domain/Value.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misp.Kernel.Domain
+{
+    public class Value
+    {
+
+        public int? oid { get; set;}
+	    public String name { get; set;}
+	
+	    public Value() {}
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="oid"></param>
+        /// <param name="name"></param>
+        public Value(int? oid, String name) {
+		    this.oid = oid;
+		    this.name = name;
+	    }
+
+        public override String ToString()
+        {
+            return name;
+        }
+
+    }
+}
