2a945267f786fb10703dce9a3ddb0caef95a3f71
Joseph <josephwambo@kps-it.com>
2016-11-19 11:55:36 +0100

    update

---
+++ misp-kernel/Resources/Styles/SidebarTreeStyle.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Misp.Kernel.Resources.Styles
+{
+    public partial class SidebarTreeStyle : ResourceDictionary
+    {
+        public void OnExpanded(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void OnClick(object sender, EventArgs e)
+        {
+
+        }
+
+        public void OnDoubleClick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
