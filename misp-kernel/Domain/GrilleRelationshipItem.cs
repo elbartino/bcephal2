55728a494bb9259fc38cf7ed7a1b8f85172733c8
Joseph <josephwambo@kps-it.com>
2016-11-21 08:00:20 +0100

    update

---
+++ misp-kernel/Domain/GrilleRelationshipItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misp.Kernel.Domain
+{
+    public class GrilleRelationshipItem : Persistent
+    {
+
+        public int position { get; set; }
+        public GrilleColumn column { get; set; }
+        public bool isExclusive { get; set; }
+
+    }
+}
---
+++ misp-kernel/Domain/GrilleRelationships.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misp.Kernel.Domain
+{
+    public class GrilleRelationships : Persistent
+    {
+
+        public PersistentListChangeHandler<GrilleRelationship> relationshipListChangeHandler { get; set; }
+
+        /**
+         * Constructor
+         */
+        public GrilleRelationships() {
+		    relationshipListChangeHandler = new PersistentListChangeHandler<GrilleRelationship>();
+	    }
+
+    }
+}
