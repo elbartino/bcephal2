3732cf4397208f155ca582d5d04adecb800befc2
Joseph <josephwambo@kps-it.com>
2016-11-30 07:47:21 +0100

    update

---
+++ misp-kernel/Ui/Office/DevExpressSheet/BSpreadsheetControl.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.Xpf.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misp.Kernel.Ui.Office.DevExpressSheet
+{
+    public class BSpreadsheetControl : SpreadsheetControl
+    {
+        protected override void OnMouseMove(System.Windows.Input.MouseEventArgs e)
+        {
+            try
+            {
+                base.OnMouseMove(e);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+    }
+}
