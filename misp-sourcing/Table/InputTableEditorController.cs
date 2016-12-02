5de79732c2756cfa7aaea914ffff857581b60aae
Joseph <josephwambo@gmail.com>
2016-12-02 16:00:56 +0100

    Update

--- misp-sourcing/Table/InputTableEditorController.cs
+++ misp-sourcing/Table/InputTableEditorController.cs
@@ -479,7 +479,7 @@ namespace Misp.Sourcing.Table
         protected OperationState saveSpreedSheet(EditorItem<InputTable> page, String fileName = null,bool saveAs = false) 
         {
             InputTableEditorItem currentPage = (InputTableEditorItem)page;
-            String excelfileName = saveAs ? buildExcelFileName(fileName) : page.EditedObject.excelFileName;
+            String excelfileName = saveAs ? buildExcelFileName(fileName) : (page.EditedObject.excelFileName + SheetConst.EXCEL_EXT);
             //page.EditedObject.excelFileName = filePath;
             //if (!string.IsNullOrEmpty(fileName)) page.EditedObject.name = Path.GetFileNameWithoutExtension(excelfileName);
             String oldFilePath = currentPage.getInputTableForm().SpreadSheet.DocumentUrl;
