48613635771484d97a89cb7695ce2f8d8db206f2
tnjipmo <thierry.njipmo@kps-it.com>
2016-12-06 17:43:28 +0100

    Updates.

--- misp-sourcing/InputGrid/InputGridEditorController.cs
+++ misp-sourcing/InputGrid/InputGridEditorController.cs
@@ -717,6 +717,7 @@ namespace Misp.Sourcing.InputGrid
 
             ((InputGridSideBar)SideBar).MeasureGroup.Tree.Click += onSelectMeasureFromSidebar;
             ((InputGridSideBar)SideBar).EntityGroup.Tree.Click += onSelectTargetFromSidebar;
+            ((InputGridSideBar)SideBar).EntityGroup.Tree.OnRightClick += onRightClickFromSidebar;
             ((InputGridSideBar)SideBar).EntityGroup.Tree.DoubleClick += onDoubleClickSelectTargetFromSidebar;
             ((InputGridSideBar)SideBar).TargetGroup.TargetTreeview.SelectionChanged += onSelectTargetFromSidebar;
 
@@ -762,7 +763,7 @@ namespace Misp.Sourcing.InputGrid
                 //    popup.ItemSource.AddRange(value.childrenListChangeHandler.Items);
                 //    popup.Tag = value;
                 //}
-                //popup. = true;
+                popup.IsOpen = true;
                 popup.Display();
             }
         }
