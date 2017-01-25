1c4ca4f88076a2cf9ee5dcd1c84ad5447bc16a62
thierry.njipmo@kps-it.com <thierry.njipmo@kps-it.com>
2017-01-25 10:23:59 +0100

    Adding missing dll.

--- misp-reporting/Report/ReportEditorController.cs
+++ misp-reporting/Report/ReportEditorController.cs
@@ -153,6 +153,11 @@ namespace Misp.Reporting.Report
             OperationState state = OperationState.CONTINUE;
             ReportEditorItem page = (ReportEditorItem)getInputTableEditor().getActivePage();
             if (page == null) return state;
+            if (page.EditedObject.template)
+            {
+                MessageDisplayer.DisplayInfo("Report", "It is not a possible to run a template");
+                return OperationState.STOP;
+            }
             nextRunActionData = null;
             TableActionData data = new TableActionData();
             if (page.EditedObject.oid.HasValue) data = new TableActionData(page.EditedObject.oid.Value, null);
