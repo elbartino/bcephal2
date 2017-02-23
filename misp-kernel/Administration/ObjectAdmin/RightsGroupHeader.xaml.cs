7b34c307a0ce57739dc3c718fb5150b37b070e93
Joseph <josephwambo@kps-it.com>
2017-02-23 14:06:31 +0100

    update

---
+++ misp-kernel/Administration/ObjectAdmin/RightsGroupHeader.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Misp.Kernel.Administration.ObjectAdmin
+{
+    /// <summary>
+    /// Interaction logic for RightsGroupHeader.xaml
+    /// </summary>
+    public partial class RightsGroupHeader : Grid
+    {
+        public RightsGroupHeader()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
