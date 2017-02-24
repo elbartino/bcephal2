06ab42311cbe447a8f6edf3b892e40f6792109a0
Joseph <josephwambo@kps-it.com>
2017-02-24 14:03:05 +0100

    Update

---
+++ misp-kernel/Ui/EditableTree/AttributeDialog.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Misp.Kernel.Ui.EditableTree
+{
+    /// <summary>
+    /// Interaction logic for AttributeDialog.xaml
+    /// </summary>
+    public partial class AttributeDialog : Window
+    {
+
+        Domain.Attribute attribute;
+        public AttributeDialog()
+        {
+            InitializeComponent();
+            this.CancelButton.Click += OnCancel;
+        }
+
+
+        public void Display(Domain.Attribute attribute)
+        {
+            this.attribute = attribute;
+            this.NameTextBox.Text = attribute.name;
+        }
+
+
+        private void OnCancel(object sender, RoutedEventArgs e)
+        {
+            this.CancelButton.Click -= OnCancel;
+            this.Close();
+        }
+
+    }
+}
