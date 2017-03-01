852b24672854c605ef8292061bcc9505108b4840
YoussLuffy <almaach_youssef@hotmail.com>
2017-03-01 14:26:33 +0100

    New screens development

--- misp-view/MainWindow.xaml.cs
+++ misp-view/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using misp_view.Views.Review;
+using misp_view.Views.Prefunding;
+using misp_view.Views.Replenishment;
 
 namespace misp_view
 {
@@ -20,9 +23,67 @@ namespace misp_view
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Review r = new Review();
+        newPrefunding p = new newPrefunding();
+        listPrefunding lp = new listPrefunding();
+        newReplenishment rp = new newReplenishment();
+        listReplenishment lrp = new listReplenishment();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void btnPrefunding_Click(object sender, RoutedEventArgs e)
+        {
+            gridInfo.Children.Clear();
+            gridInfo.Children.Add(p);
+
+        }
+
+        private void btnReview_Click(object sender, RoutedEventArgs e)
+        {
+            gridInfo.Children.Clear();
+            // Doesn't work if click more than once -> Has to be fixed   
+            gridInfo.Children.Add(r);
+        }
+
+        private void btnListPrefunding_Click(object sender, RoutedEventArgs e)
+        {
+            gridInfo.Children.Clear();
+            gridInfo.Children.Add(lp);
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            gridInfo.Children.Clear();
+            gridInfo.Children.Add(p);
+
+        }
+
+        private void MenuItem_Click2(object sender, RoutedEventArgs e)
+        {
+            gridInfo.Children.Clear();
+            gridInfo.Children.Add(lp);
+        }
+
+        private void MenuItemReplenishment_Click(object sender, RoutedEventArgs e)
+        {
+            gridInfo.Children.Clear();
+            gridInfo.Children.Add(rp);
+        }
+
+        private void MenuItemReplenishment_Click2(object sender, RoutedEventArgs e)
+        {
+            gridInfo.Children.Clear();
+            gridInfo.Children.Add(lrp);
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            gridInfo.Children.Clear();
+            // Doesn't work if click more than once -> Has to be fixed   
+            gridInfo.Children.Add(r);
         }
     }
 }
---
+++ misp-view/Views/Review/Review.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace misp_view.Views.Review
+{
+    /// <summary>
+    /// Logique d'interaction pour Review.xaml
+    /// </summary>
+    public partial class Review : UserControl
+    {
+        public Review()
+        {
+            InitializeComponent();
+        }
+
+
+        public void testFill()
+        {
+           
+
+        }
+    }
+}
