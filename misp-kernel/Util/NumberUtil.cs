8ebe2ddcaed3a7610663755b956ba1503627de79
thierry.njipmo@kps-it.com <thierry.njipmo@kps-it.com>
2017-03-09 14:44:36 +0100

    Updates.

---
+++ misp-kernel/Util/NumberUtil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misp.Kernel.Util
+{
+    public class NumberUtil
+    {
+        /// <summary>
+        /// dot as thousand separator, 
+        /// coma as decimal separator
+        /// </summary>
+        /// <param name="numberValue"></param>
+        public static string setGermanFormatter(decimal? numberValue){
+            if (numberValue == null) numberValue= 00;
+            string formatedDecimal =  numberValue.Value.ToString("N", CultureInfo.CreateSpecificCulture("de-DE"));
+            return formatedDecimal;
+        }
+    }
+}
