303e990de686a3c861c3c9d22983fb9863add661
thierry.njipmo@kps-it.com <thierry.njipmo@kps-it.com>
2017-03-09 10:27:31 +0100

    Updates.

--- misp-bfc/Base/BfcControllerFactory.cs
+++ misp-bfc/Base/BfcControllerFactory.cs
@@ -1,4 +1,5 @@
-﻿using Misp.Kernel.Application;
+﻿using Misp.Bfc.Review;
+using Misp.Kernel.Application;
 using Misp.Kernel.Controller;
 using System;
 using System.Collections.Generic;
@@ -28,8 +29,14 @@ namespace Misp.Bfc.Base
         /// <returns></returns>
         public override Controllable GetController(string fonctionality, ViewType? viewType = null, EditionMode? editionMode = null)
         {
-
-            
+            if (fonctionality == BfcFunctionalitiesCode.REVIEW)
+            {
+                ReviewBrowserController reviewController = new ReviewBrowserController();
+                reviewController.FunctionalityCode = Misp.Bfc.PlugIn.MODULE_NAME;
+                reviewController.ApplicationManager = this.ApplicationManager;
+                reviewController.Service = ((BfcServiceFactory)ServiceFactory).GetReviewService().PrefundingAccountService;
+                return reviewController;
+            }
             return null;
         }
 
--- misp-bfc/Model/PrefundingAccountData.cs
+++ misp-bfc/Model/PrefundingAccountData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Misp.Kernel.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Misp.Bfc.Model
 {
-    public class PrefundingAccountData
+    public class PrefundingAccountData :Persistent
     {
 
     }
