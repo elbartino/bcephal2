9d5e4503b4dddd5826bc3c33e100b5138ec550a7
thierry.njipmo@kps-it.com <thierry.njipmo@kps-it.com>
2017-03-10 14:53:55 +0100

    Updates.

---
+++ misp-bfc/Model/SettlementEvolutionChartData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misp.Bfc.Model
+{
+    public class SettlementEvolutionChartData
+    {
+        public string PlatForm;
+        public double value;
+        public DateTime date;
+    }
+}
