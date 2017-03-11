f055d86e7cba4655299c3fd01a7c5b8233f73cc2
thierry.njipmo@kps-it.com <thierry.njipmo@kps-it.com>
2017-03-11 10:15:46 +0100

    Updates.

---
+++ misp-bfc/Advisements/AdvisementEditorController.cs
@@ -0,0 +1,89 @@
+﻿using Misp.Bfc.Model;
+using Misp.Kernel.Application;
+using Misp.Kernel.Controller;
+using Misp.Kernel.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misp.Bfc.Advisements
+{
+    public class AdvisementEditorController : EditorController<Advisement, AdvisementBrowserData>
+    {
+
+        #region Editor and Service
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>L'Editor géré par ce controller</returns>
+        public AdvisementEditor getAdvisementEditor()
+        {
+            return (AdvisementEditor)this.View;
+        }
+               
+
+        #endregion
+
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public AdvisementEditorController()
+        {
+            ModuleName = PlugIn.MODULE_NAME;
+            this.SubjectType = SubjectType.ADVISEMENT;
+        }
+
+        public override Kernel.Application.OperationState Delete()
+        {
+            return OperationState.CONTINUE;
+        }
+
+        public override SubjectType SubjectTypeFound()
+        {
+            return SubjectType.ADVISEMENT;
+        }
+
+        public override Kernel.Application.OperationState Create()
+        {
+            return OperationState.CONTINUE;
+        }
+
+        protected override Kernel.Ui.Base.IView getNewView()
+        {
+             return new AdvisementEditor(this.SubjectType, this.FunctionalityCode); 
+        }
+
+        protected override Kernel.Ui.Base.ToolBar getNewToolBar()
+        {
+            return new Kernel.Ui.Base.ToolBar();
+        }
+
+        protected override ToolBarHandlerBuilder getNewToolBarHandlerBuilder()
+        {
+            return new ToolBarHandlerBuilder(this);
+        }
+
+        protected override Kernel.Ui.Sidebar.SideBar getNewSideBar()
+        {
+            return new Kernel.Ui.Sidebar.SideBar();
+        }
+
+        protected override Kernel.Ui.Base.PropertyBar getNewPropertyBar()
+        {
+            return null;
+        }
+
+        protected override void initializeSideBarData()
+        {
+
+        }
+
+        protected override void initializeSideBarHandlers()
+        {
+        }
+    }
+}
---
+++ misp-bfc/Advisements/AdvisementEditorItem.cs
@@ -0,0 +1,41 @@
+﻿using Misp.Bfc.Model;
+using Misp.Kernel.Domain;
+using Misp.Kernel.Ui.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Misp.Bfc.Advisements
+{
+    public class AdvisementEditorItem : EditorItem<Advisement>
+    {
+        public string DEFAULT_NAME;
+
+        public AdvisementEditorItem(Kernel.Domain.SubjectType subjectType) : base(subjectType) { }
+
+        /// <summary>
+        /// UNe nouvelle instance de la form.
+        /// </summary>
+        /// <returns></returns>
+        protected override IEditableView<Advisement> getNewEditorItemForm()
+        {
+            this.CanFloat = false;
+          
+            return new AdvisementForm(this.SubjectType);
+        }
+
+        public virtual AdvisementForm getInputTableForm()
+        {
+            return (AdvisementForm)editorItemForm;
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs args)
+        {
+
+        }
+
+        public Kernel.Service.GroupService GroupService { get; set; }
+    }
+}
