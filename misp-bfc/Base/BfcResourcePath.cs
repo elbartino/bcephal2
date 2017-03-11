9095f686d5b39e4e952cd62912dee238370048ed
Joseph <josephwambo@kps-it.com>
2017-03-11 11:46:48 +0100

    update

--- misp-bfc/Base/BfcResourcePath.cs
+++ misp-bfc/Base/BfcResourcePath.cs
@@ -11,6 +11,7 @@ namespace Misp.Bfc.Base
     {
 
         public static string BFC_RESOURCE_PATH = "bfc";
+        public static string BFC_ADVISEMENT_RESOURCE_PATH = BFC_RESOURCE_PATH + "/advisement";
         public static string BFC_REVIEW_RESOURCE_PATH = BFC_RESOURCE_PATH + "/review";
         public static string BFC_PREFUNDING_ACCOUNT_RESOURCE_PATH = BFC_REVIEW_RESOURCE_PATH + "/prefunding-account";
         public static string BFC_SETTLEMENT_EVOLUTION_RESOURCE_PATH = BFC_REVIEW_RESOURCE_PATH + "/settlement-evolution";
