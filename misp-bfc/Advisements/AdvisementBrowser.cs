f802bf0c6f8a5b3f9a5ce11e8e997f783da78ee7
Joseph <josephwambo@kps-it.com>
2017-03-11 13:29:50 +0100

    update

--- misp-bfc/Advisements/AdvisementBrowser.cs
+++ misp-bfc/Advisements/AdvisementBrowser.cs
@@ -6,74 +6,111 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using Xceed.Wpf.AvalonDock.Layout;
 
 namespace Misp.Bfc.Advisements
 {
     public class AdvisementBrowser : Browser<AdvisementBrowserData>
     {
-       public AdvisementBrowser(Kernel.Domain.SubjectType subjectType, String functionality) : base(subjectType, functionality) { }
+
+        public AdvisementBrowser(Kernel.Domain.SubjectType subjectType, String functionality) : base(subjectType, functionality) { }
 
 
-       public AdvisementType AdvisementType { get; set; }
-       public AdvisementBrowser(Kernel.Domain.SubjectType subjectType, String functionality, AdvisementType advisementType) : base(subjectType,functionality)
-       {
-           this.AdvisementType = advisementType;
-       }
+        public AdvisementType AdvisementType { get; set; }
+        public AdvisementBrowser(Kernel.Domain.SubjectType subjectType, String functionality, AdvisementType advisementType) : base(subjectType,functionality)
+        {
+            this.AdvisementType = advisementType;
+            customizeGrid();
+        }
+
+        private void customizeGrid()
+        {
+            grid.Columns.Clear();
+            for (int i = 0; i < getColumnCount(); i++)
+            {
+                DataGridColumn column = getColumnAt(i);
+                column.Header = getColumnHeaderAt(i);
+                column.Width = getColumnWidthAt(i);
+                column.IsReadOnly = isReadOnly(i);
+                if (column is DataGridBoundColumn)
+                {
+                    ((DataGridBoundColumn)column).Binding = getBindingAt(i);
+                }
+                grid.Columns.Add(column);
+            }
+            ((LayoutDocument)this.Children[0]).Title = getTitle();
+        }
+
+        protected bool isPrefunding()
+        {
+            return this.AdvisementType == AdvisementType.PREFUNDING;
+        }
+
+        protected bool isMemeber()
+        {
+            return this.AdvisementType == AdvisementType.MEMBER;
+        }
+
+        protected bool isExceptional()
+        {
+            return this.AdvisementType == AdvisementType.EXCEPTIONAL;
+        }
+
+        protected bool isSettlement()
+        {
+            return this.AdvisementType == AdvisementType.SETTLEMENT;
+        }
+
+        
 
         protected override string getTitle()
         {
-            if (this.AdvisementType.ToString().Equals(AdvisementType.MEMBER))
-                return "Member Advisement";
-
-            if (this.AdvisementType.ToString().Equals(AdvisementType.EXCEPTIONAL))
-                return "Exception Advisement";
-
-            if (this.AdvisementType.ToString().Equals(AdvisementType.SETTLEMENT))
-                return "Settlement Advisement";
-
+            if (isMemeber()) return "Member Advisement";
+            else if (isExceptional()) return "Exception Advisement";
+            else if (isSettlement()) return "Settlement Advisement";
             return "Prefunding Advisement"; 
         }
 
         protected override int getColumnCount()
         {
-            if (this.AdvisementType.ToString().Equals(AdvisementType.MEMBER))
-                return 7;
-
-            if (this.AdvisementType.ToString().Equals(AdvisementType.EXCEPTIONAL))
-                return 6;
-
-            if (this.AdvisementType.ToString().Equals(AdvisementType.SETTLEMENT))
-                return 6;
-
+            if (isExceptional() || isSettlement()) return 6;
             return 7;
         }
 
         protected override System.Windows.Controls.DataGridColumn getColumnAt(int index)
         {
-            switch (index)
+            if (isPrefunding())
             {
-                case 0: return new DataGridTextColumn();
-                case 1: return new DataGridTextColumn();
-                case 2: return new DataGridTextColumn();
-                case 3: return new DataGridTextColumn();
-                case 4: return new DataGridCheckBoxColumn();
-                case 5: return new DataGridCheckBoxColumn();
-                default: return new DataGridTextColumn();
+                switch (index)
+                {
+                    case 0: return new DataGridTextColumn();
+                    case 1: return new DataGridTextColumn();
+                    case 2: return new DataGridTextColumn();
+                    case 3: return new DataGridTextColumn();
+                    case 4: return new DataGridTextColumn();
+                    case 5: return new DataGridTextColumn();
+                    default: return new DataGridTextColumn();
+                }
             }
+            return new DataGridTextColumn();
         }
 
         protected override string getColumnHeaderAt(int index)
         {
-            switch (index)
+            if (isPrefunding())
             {
-                case 0: return "Name";
-                case 1: return "Group";
-                case 2: return "Creation Date";
-                case 3: return "Modification Date";
-                case 4: return "Active";
-                case 5: return "Template";
-                default: return "";
+                switch (index)
+                {
+                    case 0: return "Name";
+                    case 1: return "Group";
+                    case 2: return "Creation Date";
+                    case 3: return "Modification Date";
+                    case 4: return "Active";
+                    case 5: return "Template";
+                    default: return "";
+                }
             }
+            return "";
         }
 
         protected override System.Windows.Controls.DataGridLength getColumnWidthAt(int index)
@@ -92,17 +129,21 @@ namespace Misp.Bfc.Advisements
 
         protected override string getBindingNameAt(int index)
         {
-            switch (index)
+            if (isPrefunding())
             {
-                case 0: return "name";
-                case 1: return "group";
-                case 2: return "creationDateTime";
-                case 3: return "modificationDateTime";
-                case 4: return "active";
-                case 5: return "template";
-                case 6: return "visibleInShortcut";
-                default: return "oid";
+                switch (index)
+                {
+                    case 0: return "name";
+                    case 1: return "group";
+                    case 2: return "creationDateTime";
+                    case 3: return "modificationDateTime";
+                    case 4: return "active";
+                    case 5: return "template";
+                    case 6: return "visibleInShortcut";
+                    default: return "oid";
+                }
             }
+            return "";
         }
     }
 }
