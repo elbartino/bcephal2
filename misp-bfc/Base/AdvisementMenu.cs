11a10806777cef2d051367f82eb62e7a02bca6a3
thierry.njipmo@kps-it.com <thierry.njipmo@kps-it.com>
2017-03-11 14:23:20 +0100

    Updates.

--- misp-bfc/Base/AdvisementMenu.cs
+++ misp-bfc/Base/AdvisementMenu.cs
@@ -52,25 +52,25 @@ namespace Misp.Bfc.Base
         {
             this.Code = BfcFunctionalitiesCode.ADVISEMENT;
             this.Header = BfcFunctionalitiesLabel.ADVISEMENT_LABEL;
-            PrefundingAdvisementMenu = BuildMenu(BfcFunctionalitiesCode.PREFUNDING_ADVISEMENT, BfcFunctionalitiesLabel.PREFUNDING_ADVISEMENT_LABEL, BfcFunctionalitiesCode.PREFUNDING_ADVISEMENT);
+            PrefundingAdvisementMenu = BuildMenu(BfcFunctionalitiesCode.PREFUNDING_ADVISEMENT, BfcFunctionalitiesLabel.PREFUNDING_ADVISEMENT_LABEL, BfcFunctionalitiesCode.ADVISEMENT);
             NewPrefundingAdvisementMenu = BuildMenu(BfcFunctionalitiesCode.PREFUNDING_ADVISEMENT, BfcFunctionalitiesLabel.NEW_PREFUNDING_ADVISEMENT_LABEL, NavigationToken.GetCreateViewToken(BfcFunctionalitiesCode.PREFUNDING_ADVISEMENT), Kernel.Domain.RightType.CREATE);
             ListPrefundingAdvisementMenu = BuildMenu(BfcFunctionalitiesCode.PREFUNDING_ADVISEMENT, BfcFunctionalitiesLabel.LIST_PREFUNDING_ADVISEMENT_LABEL, NavigationToken.GetSearchViewToken(BfcFunctionalitiesCode.PREFUNDING_ADVISEMENT_LIST), Kernel.Domain.RightType.VIEW);
             PrefundingAdvisementMenu.Items.Add(NewPrefundingAdvisementMenu);
             PrefundingAdvisementMenu.Items.Add(ListPrefundingAdvisementMenu);
 
-            MemberAdvisementMenu = BuildMenu(BfcFunctionalitiesCode.MEMBER_ADVISEMENT, BfcFunctionalitiesLabel.MEMBER_ADVISEMENT_LABEL, BfcFunctionalitiesCode.MEMBER_ADVISEMENT);
+            MemberAdvisementMenu = BuildMenu(BfcFunctionalitiesCode.MEMBER_ADVISEMENT, BfcFunctionalitiesLabel.MEMBER_ADVISEMENT_LABEL, BfcFunctionalitiesCode.ADVISEMENT);
             NewMemberAdvisementMenu = BuildMenu(BfcFunctionalitiesCode.MEMBER_ADVISEMENT, BfcFunctionalitiesLabel.NEW_MEMBER_ADVISEMENT_LABEL, NavigationToken.GetCreateViewToken(BfcFunctionalitiesCode.MEMBER_ADVISEMENT), Kernel.Domain.RightType.CREATE);
             ListMemberAdvisementMenu = BuildMenu(BfcFunctionalitiesCode.MEMBER_ADVISEMENT, BfcFunctionalitiesLabel.LIST_MEMBER_ADVISEMENT_LABEL, NavigationToken.GetSearchViewToken(BfcFunctionalitiesCode.MEMBER_ADVISEMENT_LIST), Kernel.Domain.RightType.VIEW);
             MemberAdvisementMenu.Items.Add(NewMemberAdvisementMenu);
             MemberAdvisementMenu.Items.Add(ListMemberAdvisementMenu);
 
-            ExceptionalAdvisementMenu = BuildMenu(BfcFunctionalitiesCode.EXCEPTIONAL_ADVISEMENT, BfcFunctionalitiesLabel.EXCEPTIONAL_ADVISEMENT_LABEL, BfcFunctionalitiesCode.EXCEPTIONAL_ADVISEMENT);
+            ExceptionalAdvisementMenu = BuildMenu(BfcFunctionalitiesCode.EXCEPTIONAL_ADVISEMENT, BfcFunctionalitiesLabel.EXCEPTIONAL_ADVISEMENT_LABEL, BfcFunctionalitiesCode.ADVISEMENT);
             NewExceptionalAdvisementMenu = BuildMenu(BfcFunctionalitiesCode.EXCEPTIONAL_ADVISEMENT, BfcFunctionalitiesLabel.NEW_EXCEPTIONAL_ADVISEMENT_LABEL, NavigationToken.GetCreateViewToken(BfcFunctionalitiesCode.EXCEPTIONAL_ADVISEMENT), Kernel.Domain.RightType.CREATE);
             ListExceptionalAdvisementMenu = BuildMenu(BfcFunctionalitiesCode.EXCEPTIONAL_ADVISEMENT, BfcFunctionalitiesLabel.LIST_EXCEPTIONAL_ADVISEMENT_LABEL, NavigationToken.GetSearchViewToken(BfcFunctionalitiesCode.EXCEPTIONAL_ADVISEMENT_LIST), Kernel.Domain.RightType.VIEW);
             ExceptionalAdvisementMenu.Items.Add(NewExceptionalAdvisementMenu);
             ExceptionalAdvisementMenu.Items.Add(ListExceptionalAdvisementMenu);
 
-            SettlementAdvisementMenu = BuildMenu(BfcFunctionalitiesCode.SETTLEMENT_ADVISEMENT, BfcFunctionalitiesLabel.SETTLEMENT_ADVISEMENT_LABEL, BfcFunctionalitiesCode.SETTLEMENT_ADVISEMENT);
+            SettlementAdvisementMenu = BuildMenu(BfcFunctionalitiesCode.SETTLEMENT_ADVISEMENT, BfcFunctionalitiesLabel.SETTLEMENT_ADVISEMENT_LABEL, BfcFunctionalitiesCode.ADVISEMENT);
             NewSettlementAdvisementMenu = BuildMenu(BfcFunctionalitiesCode.SETTLEMENT_ADVISEMENT, BfcFunctionalitiesLabel.NEW_SETTLEMENT_ADVISEMENT_LABEL, NavigationToken.GetCreateViewToken(BfcFunctionalitiesCode.SETTLEMENT_ADVISEMENT), Kernel.Domain.RightType.CREATE);
             ListSettlementAdvisementMenu = BuildMenu(BfcFunctionalitiesCode.SETTLEMENT_ADVISEMENT, BfcFunctionalitiesLabel.LIST_SETTLEMENT_ADVISEMENT_LABEL, NavigationToken.GetSearchViewToken(BfcFunctionalitiesCode.SETTLEMENT_ADVISEMENT_LIST), Kernel.Domain.RightType.VIEW);
             SettlementAdvisementMenu.Items.Add(NewSettlementAdvisementMenu);
