2c81a21069bf20588a5df35f67ce7e5e9a3d7052
Joseph <josephwambo@kps-it.com>
2017-03-16 07:57:30 +0100

    update

--- misp-bfc/Model/AgeingBalanceData.cs
+++ misp-bfc/Model/AgeingBalanceData.cs
@@ -9,22 +9,22 @@ namespace Misp.Bfc.Model
     public class AgeingBalanceData
     {
 
-        public string replenishmentInstruction;
-        public string sentDate;
-        public string valueDate;
-        public string memberBankName;
-        public decimal toReceive;
-        public decimal paid;
-        public decimal pendingBalance;
-        public decimal nrftx;
-        public decimal lateCollectionAmount;
-        public decimal notReached;
-        public decimal today;
-        public decimal oneDay;
-        public decimal twoDays;
-        public decimal threeDays;
-        public decimal fourDays;
-        public decimal lessThanFourDays;
+        public string replenishmentInstruction { get; set; }
+        public string sentDate { get; set; }
+        public string valueDate { get; set; }
+        public string memberBankName { get; set; }
+        public decimal toReceive { get; set; }
+        public decimal paid { get; set; }
+        public decimal pendingBalance { get; set; }
+        public decimal nrftx { get; set; }
+        public decimal lateCollectionAmount { get; set; }
+        public decimal notReached { get; set; }
+        public decimal today { get; set; }
+        public decimal oneDay { get; set; }
+        public decimal twoDays { get; set; }
+        public decimal threeDays { get; set; }
+        public decimal fourDays { get; set; }
+        public decimal lessThanFourDays { get; set; }
 
     }
 }
