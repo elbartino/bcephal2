d9724f6778c31ba43f84908367eec7b65ab3dc57
Joseph <josephwambo@kps-it.com>
2017-03-24 15:40:24 +0100

    update

--- misp-bfc/Review/AgeingBalanceGrid.xaml.cs
+++ misp-bfc/Review/AgeingBalanceGrid.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using DevExpress.Xpf.Grid;
+using DevExpress.Xpf.Grid.ConditionalFormatting;
 using Misp.Bfc.Model;
 using System;
 using System.Collections.Generic;
@@ -72,10 +73,83 @@ namespace Misp.Bfc.Review
 
         private void InitializeHandlers()
         {
-            
+            //this.TableView.FormatConditions.Add(new FormatCondition()
+            //{                
+            //    ApplyToRow = false,
+            //    Expression = "[amountType] == 'Balance'",
+            //    FieldName = null,
+            //    Format = new Format() { Foreground = Brushes.Red }
+            //});
+            //this.TableView.FormatConditions.Add(new FormatCondition()
+            //{
+            //    ApplyToRow = true,
+            //    Expression = "[amountType] == 'Balance'",
+            //    FieldName = null,
+            //    Format = new Format() { Background = Brushes.Blue }
+            //});
+        }
+
+        private void OnCellMerge(object sender, CellMergeEventArgs e)
+        {
+            if (e.Column.FieldName == "replenishmentInstruction" || e.Column.FieldName == "sentDate"
+                || e.Column.FieldName == "valueDate" || e.Column.FieldName == "memberBankName" || e.Column.FieldName == "pml")
+            {
+                bool canBemerge = SameCellValues("replenishmentInstruction", e.RowHandle1, e.RowHandle2)
+                    && SameCellValues("sentDate", e.RowHandle1, e.RowHandle2)
+                    && SameCellValues("valueDate", e.RowHandle1, e.RowHandle2)
+                    && SameCellValues("memberBankName", e.RowHandle1, e.RowHandle2)
+                    && SameCellValues("pml", e.RowHandle1, e.RowHandle2);
+                e.Merge = canBemerge;
+            }
+            else e.Merge = false;
+            e.Handled = true;
+        }
+
+        private bool SameCellValues(String field, int row1, int row2)
+        {
+            try
+            {
+                object value1 = this.GetCellValue(row1, this.Columns[field]);
+                object value2 = this.GetCellValue(row2, this.Columns[field]);
+                return (value1 == null && value2 == null) || (value1 != null && value2 != null && value1.ToString() == value2.ToString());
+            }
+            catch (Exception) { }
+            return false;
+        }
+
+
+        private void OnCustomCellAppearance(object sender, CustomCellAppearanceEventArgs e)
+        {
+            if (e.RowSelectionState == SelectionState.None)
+            {
+                object result = e.ConditionalValue;
+                if (e.Property == TextBlock.BackgroundProperty)
+                {
+                    int n = e.RowHandle / 3;
+                    if (n % 2 == 0)
+                    {
+                        result = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FAFCFC"));
+                    }
+                }
+                if (e.Property == TextBlock.ForegroundProperty)
+                {
+                    if (e.Column != null && e.Column.FieldName != "replenishmentInstruction" && e.Column.FieldName != "sentDate"
+                        && e.Column.FieldName != "valueDate" && e.Column.FieldName != "memberBankName" && e.Column.FieldName != "pml")
+                    {
+                        if ((e.RowHandle + 1) % 3 == 0)
+                        {
+                            result = Brushes.Red;
+                        } 
+                    }
+                }
+                e.Result = result;
+                e.Handled = true;
+            }
         }
 
         #endregion
+
+        
         
     }
 }
