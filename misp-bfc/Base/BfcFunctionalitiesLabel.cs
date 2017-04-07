8f4a2521b15590fb6e8c015c4ddb218a65706e06
Joseph <josephwambo@gmail.com>
2017-04-07 18:24:23 +0100

    Update bfc menu labels

--- misp-bfc/Base/BfcFunctionalitiesLabel.cs
+++ misp-bfc/Base/BfcFunctionalitiesLabel.cs
@@ -14,29 +14,28 @@ namespace Misp.Bfc.Base
 
         public static string ADVISEMENT_LABEL = "Advisements";
 
-        public static string PREFUNDING_ADVISEMENT_LABEL = "Prefunding advisement";
-        public static string RECENT_PREFUNDING_ADVISEMENT_LABEL = "Recent prefunding advisements";
-        public static string PREFUNDING_ADVISEMENT_DASHBOARD_LABEL = "Prefunding advisements";
-        public static string NEW_PREFUNDING_ADVISEMENT_LABEL = "New prefunding advisement";
-        public static string LIST_PREFUNDING_ADVISEMENT_LABEL = "List prefunding advisements";
+        public static string PREFUNDING_ADVISEMENT_LABEL = "Pre-funding Advisement";
+        public static string RECENT_PREFUNDING_ADVISEMENT_LABEL = "Recent Pre-funding Advisements";
+        public static string PREFUNDING_ADVISEMENT_DASHBOARD_LABEL = "Pre-funding Advisements";
+        public static string NEW_PREFUNDING_ADVISEMENT_LABEL = "New Pre-funding Advisement";
+        public static string LIST_PREFUNDING_ADVISEMENT_LABEL = "List Pre-funding Advisements";
 
-        public static string MEMBER_ADVISEMENT_LABEL = "Member advisement";
-        public static string RECENT_MEMBER_ADVISEMENT_LABEL = "Recent member advisements";
-        public static string MEMBER_ADVISEMENT_DASHBOARD_LABEL = "Member advisements";
-        public static string NEW_MEMBER_ADVISEMENT_LABEL = "New member advisement";
-        public static string LIST_MEMBER_ADVISEMENT_LABEL = "List member advisements";
+        public static string MEMBER_ADVISEMENT_LABEL = "Member Advisement";
+        public static string RECENT_MEMBER_ADVISEMENT_LABEL = "Recent Member Advisements";
+        public static string NEW_MEMBER_ADVISEMENT_LABEL = "New member Advisement";
+        public static string LIST_MEMBER_ADVISEMENT_LABEL = "List Member Advisements";
 
         public static string REPLENISHMENT_INSTRUCTION_ADVISEMENT_LABEL = "Replenishment Instruction";
         public static string RECENT_REPLENISHMENT_INSTRUCTION_ADVISEMENT_LABEL = "Recent Replenishment Instructions";
         public static string REPLENISHMENT_INSTRUCTION_ADVISEMENT_DASHBOARD_LABEL = "Replenishment Instructions";
-        public static string NEW_REPLENISHMENT_INSTRUCTION_ADVISEMENT_LABEL = "New replenishment instruction";
-        public static string LIST_REPLENISHMENT_INSTRUCTION_ADVISEMENT_LABEL = "List replenishment instructions";
+        public static string NEW_REPLENISHMENT_INSTRUCTION_ADVISEMENT_LABEL = "New Replenishment Instruction";
+        public static string LIST_REPLENISHMENT_INSTRUCTION_ADVISEMENT_LABEL = "List Replenishment Instructions";
 
-        public static string SETTLEMENT_ADVISEMENT_LABEL = "Settlement advisement";
-        public static string RECENT_SETTLEMENT_ADVISEMENT_LABEL = "Recent settlement advisements";
-        public static string SETTLEMENT_ADVISEMENT_DASHBOARD_LABEL = "Settlement advisements";
-        public static string NEW_SETTLEMENT_ADVISEMENT_LABEL = "New settlement advisement";
-        public static string LIST_SETTLEMENT_ADVISEMENT_LABEL = "List settlement advisements";
+        public static string SETTLEMENT_ADVISEMENT_LABEL = "Settlement Advisement";
+        public static string RECENT_SETTLEMENT_ADVISEMENT_LABEL = "Recent Settlement Advisements";
+        public static string SETTLEMENT_ADVISEMENT_DASHBOARD_LABEL = "Settlement Advisements";
+        public static string NEW_SETTLEMENT_ADVISEMENT_LABEL = "New Settlement Advisement";
+        public static string LIST_SETTLEMENT_ADVISEMENT_LABEL = "List Settlement Advisements";
 
 
     }
